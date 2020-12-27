3196fadd9e8f488255ce2dd017705470d08abee8
Yalfoosh <headsouldev@gmail.com>
2020-12-27 19:40:19 +0100

    Dodaj DZ-02 dio 1

---
+++ dz/dz-02_/cmake-build-debug/CMakeFiles/dz_02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dz_02.dir/main.c.o"
+  "dz_02"
+  "dz_02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dz_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
